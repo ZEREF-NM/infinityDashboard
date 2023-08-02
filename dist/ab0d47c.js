4d24513681d22d5d56a26d13c2ac24579cc95363
sebastian3500 <sebastianrosa39@gmail.com>
2023-08-02 11:36:41 -0400

    commit

---
+++ dist/ab0d47c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{764:function(o,e,r){"use strict";r.r(e),e.default={pageNotFound:"La página no existe",otherError:"Ha ocurrido un error",buttonTitle:"Página de inicio",success:"ÉXITO!",textSuccess:"Tus cambios han sido guardados satisfactoriamente",error:"ERROR!",textError:"Algo extraño ha ocurrido, intentalo denuevo"}}}]);
